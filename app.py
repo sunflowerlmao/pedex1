291e06c9336a671285a8cb58ba18550d9b71c0ca
Mihail <sunflowerlmao@vk.com>
2024-05-01 22:20:02 +0700

    Initial Commit

--- app.py
+++ app.py
@@ -1,51 +1,55 @@
-import random
 
-from flask import Flask, render_template, request, redirect, url_for, current_app, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, current_app, send_from_directory, session
 import os
 from flask import jsonify
-import time
 import zipfile
 
+
 app = Flask(__name__)
-app.config['grafem'] = 'Графемы'
-app.config['ieroglif'] = 'Иероглифы'
+
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-selected_images = []
-
+selected_filenames = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    images = os.listdir(app.config['grafem'])
     return render_template('index.html')
 
-@app.route('/update_selected_images', methods=['POST'])
-def update_selected_images():
-    selected_images = request.get_json()['selectedImages']
-    # Do something with the selected_images array, e.g., store it in a database or session
-    return jsonify({'message': 'Selected images updated successfully'})
 @app.route('/images', methods=['GET', 'POST'])
 def images():
-    # Get the list of filenames from the folder
     filenames = os.listdir("static/graf/")
-
-    # Randomly select a subset of filenames
-
-
-    # Render the HTML template with the filenames
     return render_template('images.html', filenames=filenames)
+
+@app.route('/add_to_array', methods=['POST'])
+def add_to_array():
+    data = request.get_json()
+    filename = data['filename']
+    is_checked = data['is_checked']
+    if is_checked:
+        selected_filenames.append(filename)
+    else:
+        selected_filenames.remove(filename)
+    return jsonify({'status': 'success'})
+
+@app.route('/remove_from_array', methods=['POST'])
+def remove_from_array():
+    data = request.get_json()
+    filename = data['filename']
+    if filename in selected_filenames:
+        selected_filenames.remove(filename)
+    return jsonify({'status': 'success'})
 @app.route('/download', methods=['GET', 'POST'])
 def download():
     if request.method == 'POST' and request.files:
         graf = request.files["graf"]
         if graf.filename:
-            graf.save(os.path.join(app.config['grafem'], graf.filename))
-            with zipfile.ZipFile('Графемы/Графемы.zip', 'r') as zip_ref:
+            graf.save(os.path.join(app.config['UPLOAD_FOLDER'], graf.filename))
+            with zipfile.ZipFile('uploads/Графемы.zip', 'r') as zip_ref:
                 zip_ref.extractall('static/graf/')
         ier = request.files["ier"]
         if ier.filename:
-            ier.save(os.path.join(app.config['ieroglif'], ier.filename))
-            with zipfile.ZipFile('Иероглифы/Иероглифы.zip', 'r') as zip_ref:
+            ier.save(os.path.join(app.config['UPLOAD_FOLDER'], ier.filename))
+            with zipfile.ZipFile('uploads/Иероглифы.zip', 'r') as zip_ref:
                 zip_ref.extractall('static/ierog/')
         baza = request.files["baza"]
         if baza.filename:
@@ -57,11 +61,32 @@ def download():
 
     return render_template('download.html')
 
-
 @app.route('/success', methods=['GET', 'POST'])
 def success():
+    database = open('uploads/database.txt', 'r')
+    db = [x.split(':') for x in database]
+    for i in range(len(db)):
+        db[i][1] = db[i][1][:-1]
+        db[i][1] = db[i][1].split(',')
+    print(db)
+    database.close()
     return render_template('success.html')
 
+@app.route('/clear', methods=['GET', 'POST'])
+def clear():
+    q = 'static/graf'
+    w = 'static/ierog'
+    e = 'uploads/'
+    files_to_remove = [os.path.join(q, f) for f in os.listdir(q)]
+    for f in files_to_remove:
+        os.remove(f)
+    files_to_remove = [os.path.join(w, f) for f in os.listdir(w)]
+    for f in files_to_remove:
+        os.remove(f)
+    files_to_remove = [os.path.join(e, f) for f in os.listdir(e)]
+    for f in files_to_remove:
+        os.remove(f)
+    return render_template('clear.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
